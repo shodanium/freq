921a0c84b322cc36c95b8792189085e517e9a034
Andrew Aksyonoff <shodan@sphinxsearch.com>
2020-05-18 00:29:40 +0300

    added "bench.py freq01.cpp" syntax to benchmarking script

--- bench.py
+++ bench.py
@@ -5,8 +5,9 @@ if os.name != 'nt':
 	sys.exit(0)
 
 SIZE = os.path.getsize('pg.txt')
+DEF_RUNS = 10
 
-def run1(args, prog, RUNS=10):
+def run1(args, src_name, RUNS=10):
 	t = []
 	for i in range(RUNS):
 		tm = time.perf_counter()
@@ -14,11 +15,17 @@ def run1(args, prog, RUNS=10):
 		r.wait()
 		t.append(time.perf_counter() - tm)
 	t = sorted(t)
-	print('| %.3f..%.3fs | %.1f | %s |' % (t[0], t[-1], SIZE / t[0] / 1000000, prog))
+	print('| %.3f..%.3fs | %.1f | %s |' % (t[0], t[-1], SIZE / t[0] / 1000000, src_name))
 
-run1(['./bin/freq02cpp.exe'], 'freq02.cpp')
-run1(['./bin/hack01cpp.exe'], 'hack01.cpp')
-run1(['./bin/freq01cpp.exe'], 'freq01.cpp')
-run1(['./bin/freq01rs.exe'], 'freq01.rs')
-run1(['./bin/freq01go.exe'], 'freq01.go')
-run1(['php', 'freq01.php'], 'freq01.php', RUNS=3)
+runs = [
+	[['./bin/freq02cpp.exe'], 'freq02.cpp'],
+	[['./bin/hack01cpp.exe'], 'hack01.cpp'],
+	[['./bin/freq01cpp.exe'], 'freq01.cpp'],
+	[['./bin/freq01rs.exe'], 'freq01.rs'],
+	[['./bin/freq01go.exe'], 'freq01.go'],
+	[['php', './src/freq01.php'], 'freq01.php', 3]]
+
+for run in runs:
+	ok = (sys.argv[1] == run[1]) if len(sys.argv) == 2 else True
+	if ok:
+		run1(run[0], run[1], run[2] if 2 in run else DEF_RUNS)
