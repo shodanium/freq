d5ac3c07e6cd74e7a6214348d546f3280640b0ce
Andrew Aksyonoff <shodan@sphinxsearch.com>
2020-05-18 16:07:36 +0300

    fixed freq01.py vs Python 3.5

--- src/freq01.py
+++ src/freq01.py
@@ -2,8 +2,7 @@ import sys
 from collections import defaultdict
 from typing import List, Dict
 
-CHUNK_SIZE = 1 * 1024 * 1024  # 1 Мб
-
+CHUNK_SIZE = 1024 * 1024
 
 def get_words(input_file: str) -> List[str]:
     words = []
@@ -19,31 +18,24 @@ def get_words(input_file: str) -> List[str]:
                         words.append(''.join(current))
                         current = []
             chunk = f.read(CHUNK_SIZE)
-
         if len(current) > 0:
             words.append(''.join(current))
-
     return list(map(lambda w: w.lower()[:256], words))
-
 
 def get_word_frequency(words: List[str]) -> Dict[int, List[str]]:
     word_count = defaultdict(int)
     for word in words:
         word_count[word] += 1
-
     word_frequency = defaultdict(list)
     for word, count in word_count.items():
         word_frequency[count].append(word)
-
     return word_frequency
-
 
 def write_words(output_file: str, word_frequency: Dict[int, List[str]]) -> None:
     with open(output_file, 'w') as output:
         for count in sorted(word_frequency.keys(), reverse=True):
             for word in sorted(word_frequency[count]):
-                output.write(f'{count} {word}\n')
-
+                output.write(output.write(str(count) + ' ' + word + '\n')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
